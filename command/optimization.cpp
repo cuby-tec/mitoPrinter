95d38c75a6db441dd0dcdccf3f48525997200b27
Valery <walery@cuby.net>
2019-01-26 22:21:29 +0300

    cosin #8 LoadParams optimization

--- command/optimization.cpp
+++ command/optimization.cpp
@@ -2,25 +2,30 @@
 
 #include <QDebug>
 #include <assert.h>
-#include <math.h>
+
 
 #define cout    qDebug()<<__FILE__<<__LINE__
 #define MIN(A,B) A<B?A:B
 
 Optimization::Optimization()
 {
-
+	coefficien = 1.0;
+	step = 0.0;
 }
 
 void Optimization::calc(mito::Action_t &action, QQueue<sControlBlocks> &blocks)
 {
     QList<sControlBlocks>::iterator bIterator;// block iterator.
 
-    QList<ComDataReq_t>::iterator   rIterator;// request iterator.
-
     QList<sControlBlocks>::iterator agIterator;
 
+    QList<ComDataReq_t>::iterator   rIterator;// request iterator.
+
+    int index;
+
+
     int32_t acs_change_x, acs_change_y;
+
 #define BJ  acs_change_x
 #define BK  acs_change_y
 #define G4 bIterator->bb[X_AXIS].nominal_speed
@@ -69,19 +74,49 @@ void Optimization::calc(mito::Action_t &action, QQueue<sControlBlocks> &blocks)
 
     }*/
 
-//START
+    // Load optimization params
+
     bIterator = blocks.end();
-    agIterator = blocks.end();
+    --bIterator;
+
+    params[CURRENTPARAM].alfa = bIterator->bb[X_AXIS].alfa;
+    params[CURRENTPARAM].radAccel = bIterator->bb[X_AXIS].acceleration;
+    params[CURRENTPARAM].radSpeed = bIterator->bb[X_AXIS].nominal_speed; //G4
+    params[CURRENTPARAM].steps = bIterator->bb[X_AXIS].steps;
+    step = bIterator->bb[X_AXIS].steps;
+
+    --bIterator;
+    params[PREVPARAM].alfa = bIterator->bb[X_AXIS].alfa;
+    params[PREVPARAM].radAccel = bIterator->bb[X_AXIS].acceleration;
+    params[PREVPARAM].radSpeed = bIterator->bb[X_AXIS].nominal_speed; //G4
+    params[PREVPARAM].steps = bIterator->bb[X_AXIS].steps;
+
+    coefficien = 1.0;
+
+    opt(1.0);
+
+
+//START
+    index = blocks.size();
+    bIterator = blocks.end();// current source
+    agIterator = blocks.end(); // prev source
     --agIterator;
-    rIterator = action.queue.end();
+    --index;
+
+    rIterator = action.queue.end();// destination
+
+    LoadParams(*agIterator ); //load first(LAST) segment/block
+
     cout<<"Start compare ";
 
     cout<<"\nsteps\tacc_path\tspeed_path\tdecel_path\tBJx";
 
     while(bIterator != blocks.begin()){
         --bIterator;
-        if(agIterator != blocks.begin())
+        if(agIterator != blocks.begin()){
             --agIterator;
+            --index;
+        }
         --rIterator;
 
         _controlBlocks *sc = &bIterator->bb;
@@ -110,6 +145,11 @@ void Optimization::calc(mito::Action_t &action, QQueue<sControlBlocks> &blocks)
         //=IF((ABS(steps_y)>=ABS(BK34))AND(ABS(steps_x)>=ABS(BJ34)))
         bool BN = (block->bb[X_AXIS].steps >= static_cast<uint32_t>(abs(BJ))) && (block->bb[Y_AXIS].steps >= static_cast<uint32_t>(abs(BK)));
 
+        LoadParams(*agIterator);//push
+        if( isOptimized() )
+            continue;
+
+
 //        cout<<"accelerate until:"<< bIterator->bb[X_AXIS].accelerate_until <<"\tsteps:"<<bIterator->bb[X_AXIS].steps
 //           <<"\tbj:"<<acs_change_x <<"\tAGsteps:"<<agIterator->bb[X_AXIS].steps;
 
@@ -131,3 +171,139 @@ void Optimization::calc(mito::Action_t &action, QQueue<sControlBlocks> &blocks)
 
     free(opti_BI);
 }
+
+double_t Optimization::accPath(sAccPathParam* params, double_t coef) {
+	return ( pow( (coef * params->radSpeed) ,2.0)/(2.0*params->alfa*params->radAccel) );
+}
+
+double_t Optimization::stepsChandSpeed(double_t coefX) {
+//	return ( (accPath_a>0)?(accPath_a - accPath_b):(accPath_a+accPath_b));
+	double_t pathA, pathB;
+	pathA = accPath(&params[CURRENTPARAM], coefX);//19
+	pathB = accPath(&params[PREVPARAM],coefficien);//18
+
+    return ((pathA - pathB));
+}
+
+double_t Optimization::aStepsChangeSpeed(int axis)
+{
+    double_t pathA = 0.0, pathB = 0.0;
+    switch (axis)
+    {
+    case X_AXIS:
+        pathA = accPath(&aParamOptim[CURRENTPARAM].para_x, 1.0);//19
+        pathB = accPath(&aParamOptim[PREVPARAM].para_x, 1.0);//18
+        break;
+    case Y_AXIS:
+        pathA = accPath(&aParamOptim[CURRENTPARAM].para_y, 1.0);//19
+        pathB = accPath(&aParamOptim[PREVPARAM].para_y, 1.0);//18
+        break;
+    case Z_AXIS:
+        pathA = accPath(&aParamOptim[CURRENTPARAM].para_z, 1.0);//19
+        pathB = accPath(&aParamOptim[PREVPARAM].para_z, 1.0);//18
+        break;
+    case E_AXIS:
+        pathA = accPath(&aParamOptim[CURRENTPARAM].para_e, 1.0);//19
+        pathB = accPath(&aParamOptim[PREVPARAM].para_e, 1.0);//18
+        break;
+    }
+    return ((pathA - pathB));
+}
+
+//coefficient
+double_t Optimization::opt(double_t X0) {
+    double_t Xnext = 0.99 * X0;
+    double_t X1 = X0, X2;
+	double_t diff;
+
+    for(size_t i=0;i<5;i++){
+
+        X2 = X1 - fn(X1)*(X1 - Xnext)/(fn(X1)-fn(Xnext));
+
+        diff = Xnext - X2;
+        X1 = Xnext;
+        Xnext = X2;
+    }
+    X2 = X1 - fn(X1)*(X1 - Xnext)/(fn(X1)-fn(Xnext));
+    diff = Xnext - X1;
+
+	return Xnext;
+}
+
+double_t Optimization::fn(double_t X) {
+	double_t chang = stepsChandSpeed(X);
+	double_t result;
+
+	result = chang>0?(chang - step):(chang+step);
+    return result;
+}
+
+void Optimization::LoadParams(sControlBlocks &blocks)
+{
+    sParamOptim* paramoptim = &aParamOptim[CURRENTPARAM];
+    sParamOptim* prev = &aParamOptim[PREVPARAM];
+
+    memcpy(prev,paramoptim,sizeof(sParamOptim));
+
+    paramoptim->para_x.alfa = blocks.bb[X_AXIS].alfa;
+    paramoptim->para_x.steps = blocks.bb[X_AXIS].steps;
+    paramoptim->para_x.radAccel = blocks.bb[X_AXIS].acceleration;
+    paramoptim->para_x.radSpeed = blocks.bb[X_AXIS].nominal_speed;
+
+    paramoptim->para_y.alfa = blocks.bb[Y_AXIS].alfa;
+    paramoptim->para_y.steps = blocks.bb[Y_AXIS].steps;
+    paramoptim->para_y.radAccel = blocks.bb[Y_AXIS].acceleration;
+    paramoptim->para_y.radSpeed = blocks.bb[Y_AXIS].nominal_speed;
+
+    paramoptim->para_z.alfa = blocks.bb[Z_AXIS].alfa;
+    paramoptim->para_z.steps = blocks.bb[Z_AXIS].steps;
+    paramoptim->para_z.radAccel = blocks.bb[Z_AXIS].acceleration;
+    paramoptim->para_z.radSpeed = blocks.bb[Z_AXIS].nominal_speed;
+
+    paramoptim->para_e.alfa = blocks.bb[E_AXIS].alfa;
+    paramoptim->para_e.steps = blocks.bb[E_AXIS].steps;
+    paramoptim->para_e.radAccel = blocks.bb[E_AXIS].acceleration;
+    paramoptim->para_e.radSpeed = blocks.bb[E_AXIS].nominal_speed;
+
+}
+
+
+/*
+    =( ABS( INDEX(step,$A5,1)) >= ABS( INDEX(acch,$A5,1 )))
+     AND (ABS (INDEX(step,$A5,2))>= ABS( INDEX(acch,$A5,2)))
+     AND (ABS(INDEX(step,$A5,3))>=ABS( INDEX(step,$A5,3)))
+     AND (ABS( INDEX(step,$A5,4)) >=ABS( INDEX(acch,$A5,4)))
+*/
+bool Optimization::isOptimized()
+{
+    bool result = false ;
+    double_t acch;
+
+    acch = aStepsChangeSpeed(X_AXIS);
+    if(aParamOptim[CURRENTPARAM].para_x.steps >= fabs(acch))
+        result = true;  else { result = false;  }
+
+    if(result == true){
+        acch = aStepsChangeSpeed(Y_AXIS);
+        if(aParamOptim[CURRENTPARAM].para_x.steps >= fabs(acch))
+            result = true;  else { result = false;  }
+    }
+
+    if(result == true){
+        acch = aStepsChangeSpeed(Z_AXIS);
+        if(aParamOptim[CURRENTPARAM].para_x.steps >= fabs(acch))
+            result = true;  else { result = false;  }
+    }
+
+    if(result == true){
+        acch = aStepsChangeSpeed(E_AXIS);
+        if(aParamOptim[CURRENTPARAM].para_x.steps >= fabs(acch))
+            result = true;  else { result = false;  }
+    }
+
+    return result;
+}
+
+
+
+
--- command/optimization.h
+++ command/optimization.h
@@ -3,6 +3,24 @@
 
 #include "scontrolblocks.h"
 #include "gparcer/mitoaction.h"
+#include <math.h>
+
+struct sAccPathParam{
+	double_t	radSpeed;
+	double_t	radAccel;
+	double_t	alfa;
+	uint		steps;
+};
+
+struct sParamOptim{
+	sAccPathParam para_x;
+	sAccPathParam para_y;
+	sAccPathParam para_z;
+	sAccPathParam para_e;
+};
+
+#define PREVPARAM		1	// for X1
+#define CURRENTPARAM	0 	// for X0
 
 class Optimization
 {
@@ -11,6 +29,34 @@ public:
 
 //QQueue<sControlBlocks> controlblocks;
     void calc(mito::Action_t &action, QQueue<sControlBlocks>& blocks);
+
+    // acceleration path
+    double_t accPath(sAccPathParam* params, double_t coef);
+
+    //  steps for change speed
+    double_t stepsChandSpeed(double_t coefX);
+
+    double_t aStepsChangeSpeed(int axis);
+
+    // speed correction coefficient
+    double_t opt(double_t X0);
+
+    double_t fn(double_t X);
+
+    sAccPathParam params[2];
+
+    double_t step;
+
+    double_t coefficien;//18
+
+    sParamOptim aParamOptim[2];
+
+    void LoadParams( sControlBlocks & blocks);
+
+    // check array sParamOptim aParamOptim[2]
+    bool isOptimized();
+
+
 
 };
 
